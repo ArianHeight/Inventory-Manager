488b95e696cc5a2c772c720a232a550d9c17d390
ArianHeight <edmondxue@outlook.com>
2018-12-29 20:05:12 -0800

    added func to format cents into dollar

--- src/Text/TextOutput.java
+++ src/Text/TextOutput.java
@@ -9,7 +9,15 @@
 package Text;
 import java.lang.*;
 
-public class TextOutput {
+public class TextOutput 
+{
+	public TextOutput() {} //empty cstr
 	
+	public String formatCurrency(int cents) //value of something in cents, returns value of something in dollars
+	{
+		String returnValue = String.valueOf(cents); //translates the cent value into a formattable string
+		int cutIndex = returnValue.length() - 2; //name
+		return "$" + returnValue.substring(0, cutIndex) + "." + returnValue.substring(cutIndex); //splices in a period at cut index
+	}
 	
 }
