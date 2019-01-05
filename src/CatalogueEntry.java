776ca98c10182d109f84dc78c87f89e413607a0f
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-01-05 13:24:29 -0800

    Added Setters and Getters to CatalogueEntry, Removed obvious comments, tabled current comments, fixed type manufactorer->manufacturer

--- src/CatalogueEntry.java
+++ src/CatalogueEntry.java
@@ -7,25 +7,72 @@
  * */
 
 
-public class CatalogueEntry 
-{
-	// vars
+import java.util.ArrayList;
+
+public class CatalogueEntry {
 	private String m_strName;
-	private int m_iStock; //items in stock
-	private int m_iPrice; //price for sale
-	private int m_iBuyPrice; //original buying price
-	private char m_cReview; //item review
-	private String m_strManufactorer;
+	private int m_iStock;                 // Items in stock
+	private int m_iPrice;                 // Price for sale
+	private int m_iBuyPrice;              // Original buying price
+	private ArrayList<String> m_cReviews; // Item review
+	private String m_strManufacturer;     // Item's Manufacturer
 	
-	//methods
-	public CatalogueEntry(String name, int stock, int price, int bPrice, char review, String manu) 
-	{
-		//cstr
+	public CatalogueEntry(String name, int stock, int price, int bPrice, ArrayList<String> review, String manu) {
 		this.m_strName = name;
 		this.m_iStock = stock;
 		this.m_iPrice = price;
 		this.m_iBuyPrice = bPrice;
-		this.m_cReview = review;
-		this.m_strManufactorer = manu;
+		this.m_cReviews = review;
+		this.m_strManufacturer = manu;
 	}
+
+	// ------------------------ Setters and Getters (self-expanatory) ------------------------- //
+	public String getName() {
+        return m_strName;
+    }
+
+    public void setName(String name) {
+        m_strName = name;
+    }
+
+    public int getStock() {
+        return m_iStock;
+    }
+
+    public void addStock() {
+	    m_iStock++;
+    }
+
+    public void decreaseStock() {
+	    m_iStock--;
+    }
+
+    public int getPrice() {
+        return m_iPrice;
+    }
+
+    public void setPrice(int price) {
+	    m_iPrice = price;
+    }
+
+    public int getBuyPrice() {
+        return m_iBuyPrice;
+    }
+
+    public void setBuyPrice(int price) {
+	    m_iBuyPrice = price;
+    }
+
+    public ArrayList getReviews() {
+        return m_cReviews;
+    }
+
+    public void addReview(String rev) {
+	    m_cReviews.add(rev);
+    }
+
+    public String getManufactorer() {
+        return m_strManufacturer;
+    }
+    // ------------------------ End of Setters and Getters ------------------------------------ //
 }
