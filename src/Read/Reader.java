43c15b47871607d96bf9c0421733b3343aa50b01
ArianHeight <edmondxue@outlook.com>
2019-01-06 15:41:57 -0800

    added stuff to Reader.java added Reader to MainEngine.java added Data dir

--- src/Read/Reader.java
+++ src/Read/Reader.java
@@ -7,12 +7,39 @@
  * */
 
 package Read;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Reader
 {
+    private Scanner reader = new Scanner(System.in); //temporary
 
 	public Reader()
 	{
-		// TODO constructor
+		//cstr
 	}
+
+	public boolean setFileToRead(String filePath) //takes relative path and sets scanner input to it
+    {
+        try
+        {
+            reader.close(); //close the last input
+            File f = new File(System.getProperty("user.dir") + filePath); //makes new file w/ absolute path
+            reader = new Scanner(f); //makes scanner
+        }
+        catch (FileNotFoundException e)
+        {
+            //TODO catch expection here
+            System.out.println("FAILED");
+            return false; //unsuccessful return false
+        }
+
+        return true; //input change was successful, return true
+    }
+
+	public void Dstr()
+    {
+        //dstr
+    }
 }
