3e1deb119bfb3bc0f1e0caa6ac4b9fce9351b286
Arc Sofia <blackknightfortune@gmail.com>
2019-01-13 18:09:06 -0500

    Basic binSearch needs modification to satisfy dataset

--- src/Search/Searcher.java
+++ src/Search/Searcher.java
@@ -6,10 +6,47 @@
  */
 
 package Search;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+import java.lang.*;
+import java.util.Scanner;
 
 public class Searcher {
 
 	public Searcher() {
 		// TODO constructor
+	}
+
+	public int getID() //temp getter
+	{
+		int item;
+		return 0;
+	}
+
+	public int binSearch(int index, ArrayList data)
+	{
+		// dataset
+		int low = 0;
+		int high = data.size() - 1;
+		int mid;
+
+		data.get(0);
+
+		while (low <= high){
+
+			mid = (low + high) / 2;
+			if (mid > index){
+				high = mid - 1;
+			}
+			else if (mid < index){
+				low = mid + 1;
+			}
+			else
+				return mid;
+		}
+
+		return -1;
 	}
 }
